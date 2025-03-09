eee3a5027b1143fef9091a11c57d79dc9b2699cd
RikuMiyagawa <riku_miyagawa@asahi.co.jp>
2025-03-09 20:24:04 +0900

    開発用で起動できるように修正、postcss.config.cjsに変更

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-import packageJson from './package.json' assert { type: 'json' };
+// import packageJson from './package.json' assert { type: 'json' };
+
+// const version = packageJson.version;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   env: {
+//     version,
+//   },
+// };
+
+// export default nextConfig;
+
+import fs from 'fs';
+const packageJson = JSON.parse(fs.readFileSync(new URL('./package.json', import.meta.url), 'utf-8'));
 
 const version = packageJson.version;
 
